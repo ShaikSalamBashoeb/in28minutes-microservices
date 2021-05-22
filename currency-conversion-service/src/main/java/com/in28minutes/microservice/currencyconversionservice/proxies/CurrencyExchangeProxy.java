85b879bb791f3196d4dfc56b703f14333ac4a351
Shaik Salam <shaiksalam550@gmail.com>
2021-05-23 04:31:11 +0530

    Configure Load balancer on Feign. Enabled eureka client to register with eureka server.

--- currency-conversion-service/src/main/java/com/in28minutes/microservice/currencyconversionservice/proxies/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservice/currencyconversionservice/proxies/CurrencyExchangeProxy.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.in28minutes.microservice.currencyconversionservice.beans.CurrencyConversion;
 
 //name is microservice application name
-@FeignClient(name="currency-exchange", url="http://localhost:8000")
+//@FeignClient(name="currency-exchange", url="http://localhost:8000")
+//if load balancing does not happen then wait for 30 to 60 seconds and then verify it.
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange-feign/from/{from}/to/{to}")
